e22959803ea5e51caa2d44ce049c05c9998a0ff6
jcsalter <jcsalter@sc.edu>
2022-06-03 21:34:44 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 68,
+  padding: 98,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
